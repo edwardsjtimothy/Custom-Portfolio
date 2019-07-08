50a2a865bbe85bd9018a8c772d990dc001f57221
Enragedtoast <edwardsjtimothy@gmail.com>
2019-07-08 11:39:34 -0500

    basic here banner. Working on js animations

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(function() {
+    console.log( "ready!" );
+
+
+$(".hero-pic").mouseenter(function() {
+    $(".hero-pic").css("border", "2px solid #ff3300");
+});
+
+$(".hero-pic").mouseleave(function() {
+    $(".hero-pic").css("border", "2px solid #00ffff");
+
+});
+
+
+
+
+});
