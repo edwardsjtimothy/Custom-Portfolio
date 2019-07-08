fa0d979e98a43d47fbd1de2c3a67d6ceb0933d0a
Enragedtoast <edwardsjtimothy@gmail.com>
2019-07-08 12:49:25 -0500

    working on landing page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,32 @@
 $(function() {
-    console.log( "ready!" );
 
+//hover event trigger 
+
+var hover = true;
 
 $(".hero-pic").mouseenter(function() {
-    $(".hero-pic").css("border", "2px solid #ff3300");
+    
+    if (hover) {
+        var ripple = 260;
+        var wave = $("<div>");
+        wave.css({
+            "height": ripple + "px",
+            "width": ripple + "px",
+            "border-radius": "50%",
+            "border": "4px solid #00ffff"
+        });
+        console.log(wave);
+    }
+    
+   
+    
+    
 });
 
-$(".hero-pic").mouseleave(function() {
-    $(".hero-pic").css("border", "2px solid #00ffff");
+// $(".hero-pic").mouseleave(function() {
+//     $(".hero-pic").css("border", "2px solid #00ffff");
 
-});
+// });
 
 
 
