1c102f41b033d737bb08df72ebe93c6e0b35c253
Enragedtoast <edwardsjtimothy@gmail.com>
2019-07-09 12:10:54 -0500

    landing page fade in elements starting to come together!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,57 @@
-$(function() {
+$(function () {
 
-//hover event trigger 
+    //hover event trigger 
 
-var hover = true;
+    var hover = true;
 
-$(".hero-pic").mouseenter(function() {
-    
-    if (hover) {
-        var ripple = 260;
-        var wave = $("<div>");
-        wave.css({
-            "height": ripple + "px",
-            "width": ripple + "px",
-            "border-radius": "50%",
-            "border": "4px solid #00ffff"
-        });
-        console.log(wave);
-    }
-    
-   
-    
-    
+
+    $(".hero-pic").mouseenter(function () {
+
+        if (hover) {
+
+            $(".hero-pic").css({
+                "border": "2px solid #ff3300",
+                "height": "240px",
+                "width": "240px"
+            });
+
+            function wave() {
+                var ripple = 280;
+                var pos = -20;
+                var color = "#00ffff";
+
+                //alternating color of propagating rings
+                for (i = 0; i < 100; i++) {
+                    if (i % 2) {
+                        color = "#ff3300"
+                    } else {
+                        color = "#00ffff"
+                    };
+
+                    //generating ring divs
+                    var wave = $("<div>");
+                    wave.css({
+                        "height": ripple + "px",
+                        "width": ripple + "px",
+                        "border-radius": "50%",
+                        "border": "2px solid" + color,
+                        "position": "absolute",
+                        "top": pos + "px",
+                        "left": pos + "px",
+                    });
+
+                    $(".hero-content").append(wave.hide().fadeIn());
+                    ripple += 40;
+                    pos -= 20;
+                };
+            }
+            setTimeout(function () {
+                wave();
+            }, 1000);
+        }
+    });
 });
+    
 
 // $(".hero-pic").mouseleave(function() {
 //     $(".hero-pic").css("border", "2px solid #00ffff");
@@ -29,6 +59,3 @@ $(".hero-pic").mouseenter(function() {
 // });
 
 
-
-
-});
