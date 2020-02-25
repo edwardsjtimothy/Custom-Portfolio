fbd3b2256df65126e6608562ef63d3cce7b87c8b
Enragedtoast <edwardsjtimothy@gmail.com>
2020-02-24 20:33:20 -0600

    adding the rest of portfolio items to cards object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,13 @@ $(function () {
             {linkOne: "https://edwardsjtimothy.github.io/Gif-Generator/"},
             {linkTwo: "https://github.com/edwardsjtimothy/Gif-Generator"}
         ],
+        [
+            {title: "Gif Generator"},
+            {para: "This app uses the Giphy API to generate 10 gifs based on whichever button is clicked. The user can also add their own buttons. Built using Javascript, JQuery, CSS, and Bootstrap."},
+            {img: "assets/images/Gif-Generator.PNG"},
+            {linkOne: "https://edwardsjtimothy.github.io/Gif-Generator/"},
+            {linkTwo: "https://github.com/edwardsjtimothy/Gif-Generator"}
+        ],
     ];
 
     //generates portfolio cards
