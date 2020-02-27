2a243e36c8614d14775bb45fcc0c3a8c8eb1b93b
Enragedtoast <edwardsjtimothy@gmail.com>
2020-02-26 21:19:29 -0600

    corrected file path for resume

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@ $(function () {
                 if (!hover) {
                     $(".modal").modal();
                 };
-          },30000);
+          },3000);
         };
 
     //pulses ring divs
